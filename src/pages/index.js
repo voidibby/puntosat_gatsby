16c8f767ac66a8764d6307af5edafe3596371b4e
juan <juano.02@hotmail.com>
2023-02-16 00:22:44 +0100

    Modified main page and created main page styles module

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,24 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage, GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../templates/layout"
-import * as css from "../styles/header.module.scss"
+import * as css from "../styles/index.module.scss"
 
 const HomePage = () => {
   return (
     <Layout>
+      <div className={css.heroImageContainer}>{/* Here goes and image */}</div>
+      <div className={css.upcomingEvents}>
+        <h3 className={css.upcomingEventsHeading}>upcoming events</h3>
+        <ul className={css.upcomingEventsList}>
+          <li className={css.upcomingEventsListElement}>
+            <div className={css.upcomingEventsListElementImg}>{/* Here goes and image */}</div>
+            <div className={css.details}>
+              <p className={css.eventDates}>1990-1889</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
       <div className={css.test}></div>
       <div className={css.test}></div>
       <div className={css.test}></div>
