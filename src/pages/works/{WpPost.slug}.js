fa4a2d5e722074f3fc4f52df065d7c2c000ef9cb
juan <juano.02@hotmail.com>
2023-02-16 23:06:12 +0100

    Fixed wrong variable naming

--- src/pages/works/{WpPost.slug}.js
+++ src/pages/works/{WpPost.slug}.js
@@ -7,7 +7,7 @@ const WorkPage = (props, _data) => {
   const data = props.data.allWpPost.nodes[0]
   return (
     <Layout pageTitle="works">
-      <h1>{d.title}</h1>
+      <h1>{data.title}</h1>
       <Link to={`./`}>{data.title}</Link>
       <Link to={`../${data.translations[0].slug}`}>{data.translations[0].title}</Link>
       <Link to={`../${data.translations[1].slug}`}>{data.translations[1].title}</Link>
