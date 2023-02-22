ef402cecceffdd9d9e8ca448c6594c278bf1f293
juan <juano.02@hotmail.com>
2023-02-22 16:24:14 +0100

    Added attributes to image component export

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import * as css from "../styles/works.module.scss"
 
 const Image = (props) => {
   const data = useStaticQuery(graphql`
@@ -25,7 +26,19 @@ const Image = (props) => {
       {data.allFile.edges.map((edge) => {
         if (edge.node.relativePath == props.src) {
           // return <p>{edge.node.relativePath}</p>
-          return <GatsbyImage image={edge.node.childImageSharp.gatsbyImageData} />
+          return (
+            <GatsbyImage
+              image={edge.node.childImageSharp.gatsbyImageData}
+              alt="someImage"
+              layout="fullWidth"
+              placeholder="dominantColor"
+              as="div"
+              className={css.workListElementThumbnailContent}
+              imgClassName={css.workListElementThumbnailImg}
+              objectFit="cover"
+              object-position="50% 50%"
+            />
+          )
         }
       })}
     </>
